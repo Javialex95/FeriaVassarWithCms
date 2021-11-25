b9632b37e0cddb902705ac45e1a963a343999199
H3lltronik <esau.egs1@gmail.com>
2021-11-24 19:02:05 -0600

    experiences hover

--- src/components/Experience/experiences.js
+++ src/components/Experience/experiences.js
@@ -8,25 +8,25 @@ export const experiences = [
     {
         id: 1,
         title: "MÚSICA",
-        description: "una selección musical curada para generar un ambiente agradable para todos, en un espacio donde el consumo es un acto solidario. .",
+        description: "Una selección musical curada para generar un ambiente agradable para todos, en un espacio donde el consumo es un acto solidario.",
         image: music,
     },
     {
         id: 2,
         title: "PARCHES Y PUNTOS DE ENCUENTROS",
-        description: " tendremos un mobiliario muy cómodo y grandes spots instagrameables donde podrás reunirte con los tuyos a disfrutar la experiencia Vassar. .",
+        description: "Tendremos un mobiliario muy cómodo y grandes spots instagrameables donde podrás reunirte con los tuyos a disfrutar la experiencia Vassar.",
         image: parches,
     },
     {
         id: 3,
         title: "ALMACENAMIENTO PARA COMPRAS",
-        description: "en Vassar encontrarás una amplia oferta gastronómica dentro de nuestra zona de comidas. Además, dispondremos de un gran mercado en el que podrás comprar ingredientes frescos y exclusivos de nuestros emprendedores. .",
+        description: "En Vassar encontrarás una amplia oferta gastronómica dentro de nuestra zona de comidas. Además, dispondremos de un gran mercado en el que podrás comprar ingredientes frescos y exclusivos de nuestros emprendedores.",
         image: music,
     },
     {
         id: 4,
         title: "GASTRONOMÍA",
-        description: "Vassar es una feria para toda la familia. Hombres, mujeres, niños y jóvenes encontrarán experiencias y marcas con las que se sentirán identificados. .",
+        description: "Vassar es una feria para toda la familia. Hombres, mujeres, niños y jóvenes encontrarán experiencias y marcas con las que se sentirán identificados.",
         image: gastronomia,
     },
     // {
@@ -38,7 +38,7 @@ export const experiences = [
     {
         id: 6,
         title: "ESPACIO PARA TODOS",
-        description: "porque queremos que disfrutes de la feria cómodamente, hemos dispuesto una serie de lockers para que guardes tus pertenencias de forma segura. .",
+        description: "Porque queremos que disfrutes de la feria cómodamente, hemos dispuesto una serie de lockers para que guardes tus pertenencias de forma segura.",
         image: espacio,
     },
 ]

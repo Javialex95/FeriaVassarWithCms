72fa06599eb980719e252ee04d8ffe6fa4efc814
Javialex95 <alexjls95@gmail.com>
2022-06-06 09:08:54 -0500

    Ultimo

--- src/components/Faq/faqs.js
+++ src/components/Faq/faqs.js
@@ -2,36 +2,26 @@ export const faqs = [
     {
         id: 1,
         title: '¿CUÁNDO Y DÓNDE?',
-        description: 'Del ​​10 l 16 de diciembre en el Parque El Country Calle 127 # 12 en Bogotá.'
+        description: 'Descubre Vassar del 5 al 10 de Julio de 2022 en el Parque El Country de Bogotá. Calle 127 # 12.'
     },
     {
         id: 2,
-        title: '¿QUÉ ES ADEE Y A QUE SE DEDICA?',
-        description: 'Son las iniciales de la Asociación de Emprendedores, un colectivo creado pensando en un “ente” que represente al emprendimiento colombiano, sin ánimo de lucro que busca el bien común de los empresarios y sus iniciativas.'
-    },
-    {
-        id: 3,
-        title: '¿CÓMO HACER PARTE DE LA ADEE?',
-        description: 'El requisito principal es ser emprendedor y querer formar parte de este gran movimiento. Seguir a @asociaciondeemprendedores_ y enviar un mensaje directo solicitando el formulario de inscripción, el equipo de AdeE te dará toda la información que necesites y solucionará todas tus dudas.'
-    },
-    {
-        id: 4,
         title: '¿CÓMO SE ELIGEN A LOS EMPRENDIMIENTOS QUE HARÁN PARTE DE LA FERIA?',
         description: 'La curaduría de la feria será 100% realizada por la Asociación de Emprendedores (AdeE), para garantizar la transparencia en los procesos de selección de los participantes.'
     },
     {
+        id: 3,
+        title: '¿CUÁLES SON LAS CATEGORÍAS QUE SE ENCONTRARÁN EN VASSAR FERIA?',
+        description: 'Son 9 las categorías participantes, entre las cuales se encuentran moda y accesorios, mundo artesano, arte y diseño, gastronomy market, la librería, decoración y hogar, entre otros.'
+    },
+    {
+        id: 4,
+        title: '¿Cómo se puede comprar la boleta de ingreso?',
+        description: 'Ingresa a Entradas Amarillas y compra tu boleta con 25% de descuento. También podrás hacerlo en las taquillas de la entrada sin descuento.'
+    },
+    {
         id: 5,
-        title: '¿CUÁLES SON LAS CATEGORÍAS QUE SE ENCONTRARÁN EN VASSAR FERIA?',
-        description: 'Son 9 las categorías participantes, entre las cuales se encuentran moda y accesorios, mundo artesano, arte y diseño, gastronomy market, la librería, decoración y hogar, entre otros.'
-    },
-    {
-        id: 10,
-        title: '¿CUÁLES SON LAS CATEGORÍAS QUE SE ENCONTRARÁN EN VASSAR FERIA?',
-        description: 'Son 9 las categorías participantes, entre las cuales se encuentran moda y accesorios, mundo artesano, arte y diseño, gastronomy market, la librería, decoración y hogar, entre otros.'
-    },
-    {
-        id: 11,
-        title: '¿CUÁNDO EMPIEZAN LAS VENTAS DE BOLETERÍA?',
-        description: 'El 23 de noviembre a través de entradas amarillas.com'
-    },
+        title: '¿Qué artistas musicales estarán participando en esta versión?',
+        description: 'Tendremos shows increíbles que estaremos anunciado cada día, no te pierdas estos shows y pre compra tus entradas con tiempo y asegura tu espacio.'
+    }
 ]
